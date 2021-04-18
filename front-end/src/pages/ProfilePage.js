c65bab659c7eac48e99fe0edc63e920a1fc60cb3
realwhizzylong <therealwhizzy@gmail.com>
2021-04-19 00:05:26 +0100

    fix bugs

--- front-end/src/pages/ProfilePage.js
+++ front-end/src/pages/ProfilePage.js
@@ -58,7 +58,7 @@ const ProfilePage = ({ history }) => {
                     <Form.Group controlId="name">
                         <Form.Label>Name</Form.Label>
                         <Form.Control
-                            type="name"
+                            type="text"
                             placeholder="Enter name"
                             value={name}
                             onChange={e => setName(e.target.value)}
